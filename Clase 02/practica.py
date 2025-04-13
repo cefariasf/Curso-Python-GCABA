580f9eb1884e2bd936e81e3df0ee7503766a7487
cefariasf <cecilia.fariasf@gmail.com>
2025-04-13 20:48:45 -0300

    Práctica Comandos Python

---
+++ Clase 02/practica.py
@@ -0,0 +1,54 @@
+# Definition de variables
+nombre = "Cecilia"
+primer_nombre = "Maria"
+segundo_nombre = "Irene"
+
+nombre, apellido = "Cecilia", "Farias Franzosi"
+
+
+# TIPOS DE DATOS
+
+cadenaCaracteres = "Texto"
+
+numeroEntero = 15
+
+numeroDecimal = 15.35
+
+boolean = True or False
+
+
+print(nombre)
+
+print(primer_nombre, cadenaCaracteres)
+
+
+print("Hola\nMundo\nCruel")
+
+print(cadenaCaracteres + "\n" + primer_nombre)
+
+#INPUT
+
+apellido = input("Ingresa tu apellido: ")
+
+print("El apellido ingresado es",apellido)
+
+
+edad = str(10)
+
+print(edad)
+
+# OPERACIONES ARITMÉTICAS
+
+
+suma = 10+5
+
+resta = 20-4
+
+division = 40/4
+
+multiplicacion = 50*1
+
+print(suma)
+print(resta)
+print(division)
+print(multiplicacion)

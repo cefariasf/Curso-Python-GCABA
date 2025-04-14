d8d872568e2ff5473d8d9890db522bb9a1bd75f3
cefariasf <cecilia.fariasf@gmail.com>
2025-04-13 21:55:18 -0300

    Pruebas_Clase_2

---
+++ Clase 02/practica2.py
@@ -0,0 +1,29 @@
+# valor = bool(0)
+# print (valor)
+
+# nombre = "Juan"
+
+# edad = 30
+
+# print("Hola,", nombre, "tenés", edad, "años.")
+
+# edad = input("Ingresá tu edad: ")
+
+# edad = int(edad)
+
+# print("Tu edad es:", edad)
+
+# x, y, z = 1, 2, 3
+
+# print(x + y + z)
+
+# print (x)
+# print (y)
+# print (z)
+
+# edad = 25.5
+
+# print(int(edad) + 5)
+
+resultado = 7 // 3
+print (resultado)

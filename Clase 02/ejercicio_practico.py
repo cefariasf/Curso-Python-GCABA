683c7c9cefec7f4b1ca796f69eeca1fa4411ee44
cefariasf <cecilia.fariasf@gmail.com>
2025-04-13 21:55:03 -0300

    EjercicioPractico_Clase_2

---
+++ Clase 02/ejercicio_practico.py
@@ -0,0 +1,7 @@
+comienzo = "Ingrese su "
+nombre = input(comienzo+"nombre: ")
+apellido = input(comienzo+"apellido: ")
+edad = input(comienzo+"edad: ")
+correo_electronico = input(comienzo+"correo electrónico: ")
+
+print("Soy " + nombre +" "+ apellido + ", tengo " + edad + " años, y mi mail es " + correo_electronico)

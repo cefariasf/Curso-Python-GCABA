31d9d90725c6316c5871d7fb67c9d6b1b240a5c5
cefariasf <cecilia.fariasf@gmail.com>
2025-04-14 20:48:11 -0300

    PracticaClase3

---
+++ Clase03/practica.py
@@ -0,0 +1,19 @@
+"""
+
+print("hola" == "Hola")
+
+usuario_ingresado = input("Ingresa tu usuario: ")
+contraseña_ingresada = input("Ingresa tu contraseña: ")
+
+usuario_db = "checho123"
+contraseña_db = "pupi456"
+
+print( usuario_ingresado == usuario_db ) and ( contraseña_ingresada == contraseña_db )
+
+"""
+rol_ingresado = input("Ingresa el rol que tenés: ")
+
+rol1 = "Administrador"
+rol2 = "Moderador"
+
+print(rol_ingresado == rol1 or rol_ingresado == rol2)

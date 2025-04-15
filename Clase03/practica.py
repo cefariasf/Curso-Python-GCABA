b561b6af13c9c8ff273457ba78d82ef7884727d4
cefariasf <cecilia.fariasf@gmail.com>
2025-04-14 21:30:47 -0300

    IfAndElse

--- Clase03/practica.py
+++ Clase03/practica.py
@@ -11,9 +11,37 @@ contraseña_db = "pupi456"
 print( usuario_ingresado == usuario_db ) and ( contraseña_ingresada == contraseña_db )
 
 """
-rol_ingresado = input("Ingresa el rol que tenés: ")
+# rol_ingresado = input("Ingresa el rol que tenés: ")
 
-rol1 = "Administrador"
-rol2 = "Moderador"
+# rol1 = "Administrador"
+# rol2 = "Moderador"
 
-print( rol_ingresado == rol1 or rol_ingresado == rol2)
+# print( rol_ingresado == rol1 or rol_ingresado == rol2)
+
+# num = 10
+
+# if num == 5:
+#     print("El número ingresado es correcto")
+#     print(num*2)
+#     print(num/2)
+#     print(num**2)
+   
+#     print("Éstas son un conjunto de operaciones en donde opera al num con el 2")
+# else:
+#     print("El número es incorrecto")
+
+usuario_ingresado = input("Ingresa tu usuario: ")
+contraseña_ingresado = input("Ingresa tu contraseña: ")
+
+usuario_db = "chech123"
+contraseña_db = "pupi456"
+
+if ((usuario_ingresado == usuario_db and contraseña_ingresado == contraseña_db)):
+    print("Bienvenido, ingresaste correctamente")
+
+else:
+    print("Alguno de tus datos es incorrecto")
+    if usuario_ingresado == usuario_db:
+        print("Hola, ", usuario_ingresado, ", la contraseña que ingresaste es incorrecta")
+    else:
+        print("Esa cuenta no existe")
